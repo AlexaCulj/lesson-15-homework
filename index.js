8f21fd5f2f76e6e66ee4e56eaeffed7bf8020358
AlexaCulj <43860244+AlexaCulj@users.noreply.github.com>
2019-03-20 22:48:00 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+var a = prompt("Enter a");
+var b = prompt("Enter b");
+var c = prompt("Enter c");
+
+function calcdiscrim(a, b, c) {
+    
+    return b * b - 4 * a * c;
+}
+function calculateRoots1(x, x1, x2) {
+    
+    if (discrim < 0) {
+        return x = " Нет действительных корней";
+
+    } else if (discrim = 0) {
+        return x = -b / 2 * a;
+        
+     } else (discrim > 0); {
+         return x1,x2 = -b +- Math.sqrt(discrim) / 2 * a;
+        
+     }
+}
